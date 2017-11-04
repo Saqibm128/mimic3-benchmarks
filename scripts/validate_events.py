36596d6372bc7df8a454e475cf8392a927b60532
Mohammed Saqib <msaqib3@gatech.edu>
2017-11-04 15:07:37 -0400

    started work on trying to make validateEvents use pandas instead of manual for loops

--- scripts/validate_events.py
+++ scripts/validate_events.py
@@ -1,6 +1,7 @@
 import os
 import argparse
-
+import pandas as pd
+import numpy as np
 
 def is_subject_folder(x):
     for c in x:
@@ -10,6 +11,18 @@ def is_subject_folder(x):
 
 
 def main():
+    #Set up some variables for verbose logging
+    bad_pairs = set()
+    missing_events = [];
+    
+    n_events = 0
+    emptyhadm = 0
+    noicustay = 0
+    recovered = 0
+    couldnotrecover = 0
+    icustaymissinginstays = 0
+    nohadminstay = 0
+    
     parser = argparse.ArgumentParser()
     parser.add_argument('subjects_root_path', type=str,
                         help='Directory containing subject sub-directories.')
@@ -22,74 +35,27 @@ def main():
     # get mapping for subject
     maps = {}
     for (index, subject) in enumerate(subjects):
-        with open(os.path.join(args.subjects_root_path, subject, "stays.csv")) as f:
-            rows = f.readlines()[1:]
-            for row in rows:
-                hadm_id = row.split(',')[1]
-                icustay_id = row.split(',')[2]
-                assert hadm_id != ""
-                assert icustay_id != ""
+        staysDF = pd.DataFrame.from_csv(os.path.join(args.subjects_root_path, subject, "stays.csv"))
+        staysDF.columns = staysDF.columns.str.upper()
+        staysDF.dropna(axis=0, how="any", subset=["HADM_ID"])
+        for subject_id in staysDF.index:
+            if not np.isnan(subject_id):
+                subject = staysDF["SUBJECT"][row]
+                hadm_id = staysDF["HADM_ID"][row]
                 if (not subject in maps):
                     maps[subject] = dict()
                 maps[subject][hadm_id] = icustay_id
-    
-    bad_pairs = set()
-    
-    n_events = 0
-    emptyhadm = 0
-    noicustay = 0
-    recovered = 0
-    couldnotrecover = 0
-    icustaymissinginstays = 0
-    nohadminstay = 0
-    
-    for (index, subject) in enumerate(subjects):
-        new_lines = []
-        with open(os.path.join(args.subjects_root_path, subject, "events.csv")) as f:
-            lines = f.readlines()
-            header = lines[0]
-            lines = lines[1:]
-            new_lines.append(header)
-            for line in lines:
-                mas = line.split(',')
-                hadm_id = mas[1]
-                icustay_id = mas[2]
-                
-                n_events += 1
-                if (hadm_id == ""):
-                    emptyhadm += 1
-                    continue
-                
-                try:
-                    icustay_id_from_maps = maps[subject][hadm_id]
-                
-                    if (icustay_id == ""):
-                        noicustay += 1
-                        try:
-                            icustay_id = maps[subject][hadm_id]
-                            recovered += 1
-                            new_line = ','.join(mas[:2] + [icustay_id] + mas[3:])
-                            new_lines.append(new_line)
-                        except:
-                            couldnotrecover += 1
-                    else:
-                        if icustay_id != icustay_id_from_maps:
-                            icustaymissinginstays += 1
-                        else:
-                            new_line = ','.join(mas[:2] + [icustay_id] + mas[3:])
-                            new_lines.append(new_line)
-                except:
-                    nohadminstay += 1
-                    bad_pairs.add((subject, hadm_id))
-                    
-        
-        with open(os.path.join(args.subjects_root_path, subject, "events.csv"), "w") as f:
-            for new_line in new_lines:
-                f.write(new_line)
-        
         if (index % 100 == 0):
             print "processed %d / %d" % (index+1, len(subjects)), "         \r",
-    print ""    
+            print ""   
+        ##For some reason no events file was written
+        if os.path.isfile(os.path.join(args.subjects_root_path, subject, "events.csv")): 
+            eventsDF = pandas.DataFrame.from_csv(os.path.join(args.subjects_root_path, subject, "events.csv"))
+            eventsDF = eventsDF.dropna(axis=0, how="any", subset=["ICUSTAY_ID"])
+            joined = eventsDF.join(staysDF, on=["SUBJECT_ID", "HADM_ID"], how="left", rsuffix="_r")
+            joined.to_csv(os.path.join(args.subjects_root_path, subject, "events2.csv"))
+    
+         
     
     #print bad_pairs
     print('n_events', n_events,
@@ -98,7 +64,8 @@ def main():
         'recovered', recovered ,
         'couldnotrecover', couldnotrecover ,
         'icustaymissinginstays', icustaymissinginstays ,
-        'nohadminstay', nohadminstay )
+        'nohadminstay', nohadminstay,
+        'noevents', missing_events )
         
 if __name__=="__main__":
     main()
